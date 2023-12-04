e7e19c5dc5ee85f27438788841796d37ff0d1b02
Ripred <anuarsaenger.2001@gmail.com>
2023-12-04 01:16:06 -0500

    added description

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-pub mod digital_canvas;
+/// A Rust library for representing 2D planes of RGB values
+/// Developed for use with microcontrollers driving LED pannels
 
+pub mod digital_canvas;
 pub use self::digital_canvas::DigitalCanvas;
 pub use self::digital_canvas::RgbPixel;
